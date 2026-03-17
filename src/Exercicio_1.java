cbc0703f1f5538fbb9ac2d5a3ed458f079fdea52
jhean <jhean.soares@academico.ifpb.edu.br>
2026-03-17 14:49:56 -0300

    Questão 1 finalizada

--- src/Exercicio_1.java
+++ src/Exercicio_1.java
@@ -1,3 +1,16 @@
 void main(){
+    String nome = IO.readln("Escreva seu nome: ");
 
+    double nota1 = Double.parseDouble(IO.readln("Escreva sua primeira nota: "));
+    double nota2 = Double.parseDouble(IO.readln("Escreva sua segunda nota: "));
+    double nota3 = Double.parseDouble(IO.readln("Escreva sua terceira nota: "));
+
+    double media = (nota1+nota2+nota3*2)/4;
+    System.out.printf("A sua média foi %.2f.\n",media);
+
+    if(media > 7) {
+        IO.println("Você está aprovado.");
+    } else {
+        IO.println("Você está reprovado.");
+    }
 }
